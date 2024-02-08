3e86ea6aed8f5a4a15ecce3a943fb32c32c349e1
erenof <erenof79@gmail.com>
2024-02-08 11:02:43 -0300

    first commit

---
+++ Ex1/Excercise_1.py
@@ -0,0 +1,11 @@
+# This program says hello and asks for my name
+
+print("Hello World!")
+print("Whats is your name?") # Asks for their name
+myName = input()
+print("It's good to meet you, " + myName)
+print("The length of your name is: ")
+print(len(myName))
+print("What is your age?") # Asks for their age
+myAge = input()
+print("You will be " + str(int(myAge) +1) + " in a year.")
